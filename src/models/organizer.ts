45f32d66798c33242a5cb07e9d5dbdea5686268d
Nick van Raaij <dagelijksnick@gmail.com>
2021-09-09 21:55:58 +0200

    added sequelize models

---
+++ src/models/organizer.ts
@@ -0,0 +1,28 @@
+import { Table, Column, Model, PrimaryKey, IsUUID, AllowNull, HasMany } from 'sequelize-typescript';
+import { Voter } from './voter';
+
+@Table({
+  paranoid: true,
+  defaultScope: {
+    attributes: {
+      exclude: [
+        'createdAt',
+        'updatedAt',
+        'deletedAt',
+      ],
+    },
+  },
+})
+export class Organizer extends Model {
+  @IsUUID(4)
+  @PrimaryKey
+  @Column
+  id: string;
+
+  @AllowNull(false)
+  @Column
+  name: string
+
+  @HasMany(() => Voter)
+  voters: Voter[]
+}
---
+++ src/models/voter.ts
@@ -0,0 +1,45 @@
+import { AllowNull, BelongsTo, Column, ForeignKey, IsUUID, Model, PrimaryKey, Table } from 'sequelize-typescript';
+import { Organizer } from './organizer';
+
+@Table({
+  paranoid: true,
+  defaultScope: {
+    attributes: {
+      exclude: [
+        'createdAt',
+        'updatedAt',
+        'deletedAt',
+      ],
+    },
+  },
+  scopes: {
+    jwt: {
+      attributes: [
+        'id',
+        'name',
+        'type',
+      ],
+    },
+  },
+})
+export class Voter extends Model {
+    @IsUUID(4)
+    @PrimaryKey
+    @Column
+    id: string;
+
+    @AllowNull(false)
+    @Column
+    email: string
+
+    @AllowNull(false)
+    @Column
+    votingPin: string;
+
+    @ForeignKey(() => Organizer)
+    @Column
+    organizerId: string
+
+    @BelongsTo(() => Organizer)
+    organizer: Organizer
+}
